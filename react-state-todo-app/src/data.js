c1bbd05f82443f98b2f347cf6097f7d57aa2c47c
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-09-19 14:54:08 -0400

    Create data.js

---
+++ react-state-todo-app/src/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id: 1,
+        text: "Learn useState",
+        genre: "learning",
+        done: false
+    },
+    {
+        id: 2,
+        text: "Build a todo app",
+        genre: "project",
+        done: false
+    },
+    {
+        id: 3,
+        text: "Read the React docs",
+        genre: "learning",
+        done: true
+    },
+    {
+        id: 4,
+        text: "Push code to GitHub",
+        genre: "project",
+        done: false
+    }
+]
+
+export default data
