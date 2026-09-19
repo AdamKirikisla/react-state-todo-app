bf10d812115e8015709293c181fcb0f83b5746d2
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-09-19 14:47:52 -0400

    create AddTaskForm.jsx

---
+++ react-state-todo-app/src/Components/AddTaskForm.jsx
@@ -0,0 +1,36 @@
+export default function(){
+    return(
+                    <form action="">
+                <input type="text"placeholder="Add a task..." className="inpute-text"/>
+
+                
+                <div className="radio-inputs">
+
+                    <label className="radio">
+                        <input type="radio" name="priority" value="low"/>
+                        <span className="radio-text">Low</span>
+                    </label>
+
+                    <label className="radio">
+                        <input type="radio" name="priority" value="medium" defaultChecked/>
+                        <span className="radio-text">Medium</span>
+                    </label>
+
+                    <label className="radio">
+                        <input type="radio" name="priority" value="high"/>
+                        <span className="radio-text">High</span>
+                    </label>
+
+                    <label className="radio">
+                        <input type="radio" name="priority" value="urgent"/>
+                        <span className="radio-text">Urgent</span>
+                    </label>
+
+
+                </div>
+
+                <button type="submit" className="form-button">↵</button>
+            
+            </form>
+    )
+}
--- react-state-todo-app/src/Components/Main.jsx
+++ react-state-todo-app/src/Components/Main.jsx
@@ -1,40 +1,17 @@
+import AddTaskForm from "./AddTaskForm"
+
 export default function Main(){
     return(
         <main>
-            <form action="">
-                <input type="text"placeholder="Add a task..." className="inpute-text"/>
-
-                
-                <div className="radio-inputs">
-
-                    <label className="radio">
-                        <input type="radio" name="priority" value="low"/>
-                        <span className="radio-text">Low</span>
-                    </label>
-
-                    <label className="radio">
-                        <input type="radio" name="priority" value="medium" defaultChecked/>
-                        <span className="radio-text">Medium</span>
-                    </label>
-
-                    <label className="radio">
-                        <input type="radio" name="priority" value="high"/>
-                        <span className="radio-text">High</span>
-                    </label>
-
-                    <label className="radio">
-                        <input type="radio" name="priority" value="urgent"/>
-                        <span className="radio-text">Urgent</span>
-                    </label>
-
-
-                </div>
-
-                <button type="submit" className="form-button">↵</button>
-            
-            </form>
+            <AddTaskForm />
 
             <div className="filter-priority">
+
+                 <label>
+                    <input type="radio" name="filter" value="all"/>
+                    <span>All</span>
+                </label>
+                
                 <label>
                     <input type="radio" name="filter" value="low"/>
                     <span>Low</span>
