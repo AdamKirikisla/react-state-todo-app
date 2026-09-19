15ee56e52275033f8f8fd8bdea97eb75f49b9fbd
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-09-19 14:11:43 -0400

    add Main component with forms

---
+++ react-state-todo-app/src/Components/Main.jsx
@@ -0,0 +1,13 @@
+export default function Main(){
+    return(
+        <main>
+            <form action="">
+                <input type="text"placeholder="Add a task..." />
+
+                
+                
+                
+            </form>
+        </main>
+    )
+}
