5140a07bcc44db3b345e6d2e091c51df33fc7516
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-09-19 20:21:14 -0400

    create TaskList.jsx

--- react-state-todo-app/src/Components/Main.jsx
+++ react-state-todo-app/src/Components/Main.jsx
@@ -1,11 +1,13 @@
 import AddTaskForm from "./AddTaskForm"
 import PriorityFilter from "./PriorityFilter"
+import TaskList from "./TaskList"
 
 export default function Main(){
     return(
         <main>
             <AddTaskForm />
             <PriorityFilter />
+            <TaskList />
 
             
         </main>
---
+++ react-state-todo-app/src/Components/TaskList.jsx
@@ -0,0 +1,7 @@
+export default function TaskList(){
+    return(
+        <section>
+            <h2>Hello from task list</h2>
+        </section>
+    )
+}
