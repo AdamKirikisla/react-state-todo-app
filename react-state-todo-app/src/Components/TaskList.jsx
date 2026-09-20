ff3597de8623fa8d793aaaa74fb9b411173c54dc
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-09-20 15:31:41 -0400

    add styling to tasks

--- react-state-todo-app/src/Components/TaskList.jsx
+++ react-state-todo-app/src/Components/TaskList.jsx
@@ -5,13 +5,11 @@ import ToggleButton from "./ToggleButton"
 export default function TaskList(props){
     
     const listTasks = props.tasks.map(
-        task => (<li key={task.id} className={task.done ? "task-done" : "task"}>
+        task => (<li key={task.id} className={task.done ? "task task-done" : "task"}>
             <ToggleButton checked={task.done} onClick={() => props.completeTask(task.id)} />
-            {task.text} {task.priority}
+            <span className="task-text">{task.text}</span>
+            <span className={`task-priority priority-${task.priority}`}>{task.priority}</span>
             <DeleteButton onClick={() => props.deleteTask(task.id)}/>
-            
-            
-        
         </li>)
     )
     
