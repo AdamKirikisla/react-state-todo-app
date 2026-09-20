33a5b3b22a28dcc739f7d764388c445b493b9843
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-09-19 21:59:21 -0400

    style task item

--- react-state-todo-app/src/Components/TaskList.jsx
+++ react-state-todo-app/src/Components/TaskList.jsx
@@ -2,18 +2,17 @@
 export default function TaskList(props){
     
     const listTasks = props.tasks.map(
-        task => (<li key={task.id}>
-            <button>&#x2610;</button>
+        task => (<li key={task.id} className="task">
+            <button className="finish-task">&#x2610;</button>
             {task.text} {task.priority}
-            <button onClick={() => props.deleteTask(task.id)}>&#x2613;</button>
+            <button className="delete-task" onClick={() => props.deleteTask(task.id)}>&#x2613;</button>
         
         </li>)
     )
     
     return(
         <section>
-            <h2>Hello from task list</h2>
-            <ul>
+            <ul className="task-list">
                 {listTasks}
 
             </ul>
