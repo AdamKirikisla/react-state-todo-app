27216ac0ec8ca36945679d8ae892208f75dfda74
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-09-20 15:28:23 -0400

    add 'ToggleButton.jsx component and style'

--- react-state-todo-app/src/Components/TaskList.jsx
+++ react-state-todo-app/src/Components/TaskList.jsx
@@ -1,11 +1,12 @@
 import DeleteButton from "./DeleteButton"
+import ToggleButton from "./ToggleButton"
 
 
 export default function TaskList(props){
     
     const listTasks = props.tasks.map(
         task => (<li key={task.id} className={task.done ? "task-done" : "task"}>
-            <button className="finish-button" onClick={() => props.completeTask(task.id)}>&#x2610;</button>
+            <ToggleButton checked={task.done} onClick={() => props.completeTask(task.id)} />
             {task.text} {task.priority}
             <DeleteButton onClick={() => props.deleteTask(task.id)}/>
             
