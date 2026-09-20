3668877a4d92735e3ea4bce8d2e0d2dd036e4999
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-09-19 20:42:17 -0400

    display data in TaskList.jsx

--- react-state-todo-app/src/Components/Main.jsx
+++ react-state-todo-app/src/Components/Main.jsx
@@ -1,13 +1,20 @@
 import AddTaskForm from "./AddTaskForm"
 import PriorityFilter from "./PriorityFilter"
 import TaskList from "./TaskList"
+import data from "../data.js"
+import { useState } from "react"
+
 
 export default function Main(){
+
+    const [todoData, setTodoData] = useState(data)
+
     return(
         <main>
             <AddTaskForm />
             <PriorityFilter />
-            <TaskList />
+            <TaskList tasks={todoData} />
+            
 
             
         </main>
--- react-state-todo-app/src/Components/TaskList.jsx
+++ react-state-todo-app/src/Components/TaskList.jsx
@@ -1,7 +1,17 @@
-export default function TaskList(){
+
+export default function TaskList(props){
+    
+    const listTasks = props.tasks.map(
+        task => (<li key={task.id}>{task.text} {task.priority}</li>)
+    )
+    
     return(
         <section>
             <h2>Hello from task list</h2>
+            <ul>
+                {listTasks}
+
+            </ul>
         </section>
     )
 }
